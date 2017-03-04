d72a5e6bff11cc93adfcd1b01881bd6d957bed1f
Richard Seal <RSeal@rbaconsulting.com>
2017-03-04 02:39:40 -0500

    Stubbed out the IFaceApiService contract

---
+++ src/Feature/CognativeServices/code/Services/IFaceApiService.cs
@@ -0,0 +1,9 @@
+﻿namespace Sitecore.Feature.CognitiveServices.Services
+{
+    public interface IFaceApiService
+    {
+        string CreatePerson();
+        void AddPhotoToPerson();
+        string VerifyPerson();
+    }
+}
