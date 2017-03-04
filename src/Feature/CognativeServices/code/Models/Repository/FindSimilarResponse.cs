3262ff59e472c9ca01a8f63613daacdfd6916bb3
rba\jwilkerson <jason.wilkerson@rbaconsulting.com>
2017-03-04 14:21:50 -0600

    Adding initial implementation of a few methods in IFaceApiService

---
+++ src/Feature/CognativeServices/code/Models/Repository/FindSimilarResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Sitecore.Feature.CognitiveServices.Models.Repository
+{
+   [Serializable]
+   public class FindSimilarResponse
+   {
+      [JsonProperty("persistedFaceId")]
+      public Guid FaceId { get; set; }
+      [JsonProperty("confidence")]
+      public double Confidence { get; set; }
+   }
+}
---
+++ src/Feature/CognativeServices/code/Models/Repository/VerifyResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Sitecore.Feature.CognitiveServices.Models.Repository
+{
+   [Serializable]
+   public class VerifyResponse
+   {
+      [JsonProperty("isIdentical")]
+      public bool IsIdentical { get; set; }
+      [JsonProperty("confidence")]
+      public double Confidence { get; set; }
+      [JsonProperty("error")]
+      public Error Error { get; set; }
+   }
+}
