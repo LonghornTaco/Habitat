47acf1ad2e74ae3c1ccc2924d366d21959e8643f
Jean-François L'Heureux <spot_web@hotmail.com>
2017-03-04 05:19:34 -0500

    Webcam capture of a JPEG file in Sitecore Hello page

--- src/Feature/CognativeServices/code/Scripts/CognitiveServices/CognitiveServices.js
+++ src/Feature/CognativeServices/code/Scripts/CognitiveServices/CognitiveServices.js
@@ -1,1 +1,83 @@
-﻿
+﻿var webcamCapture = {
+    videoTracks: undefined,
+
+    initialize: function (options) {
+        this.webcamAccessErrorSectionElement = document.getElementById(options.webcamAccessErrorSection);
+        this.videoPlayerElement = document.getElementById(options.videoPlayer);
+        this.snapshotCanvasElement = document.getElementById(options.snapshotCanvas);
+
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(this.attachWebcamStreamToVideoPlayer.bind(this))
+            .then(this.hideWebcamAccessErrorSection.bind(this))
+            .then(this.displayVideoPlayer.bind(this))
+            .catch(this.handleWebcamAccessError.bind(this));
+    },
+
+    teardown: function () {
+        this.stopWebcamStream();
+        this.hideWebcamAccessErrorSection();
+        this.hideVideoPlayer();
+        this.hideSnapshot();
+    },
+
+    hide: function (element) {
+        element.classList.add("hellohidden");
+    },
+
+    show: function (element) {
+        element.classList.remove("hellohidden");
+    },
+
+    attachWebcamStreamToVideoPlayer: function (stream) {
+        // Attach the video stream to the video element and autoplay.
+        this.videoPlayerElement.srcObject = stream;
+        this.videoTracks = stream.getVideoTracks();
+    },
+
+    captureSnapshot: function () {
+        var context = this.snapshotCanvasElement.getContext('2d');
+        context.drawImage(this.videoPlayerElement, 0, 0, this.snapshotCanvasElement.width, this.snapshotCanvasElement.height);
+
+        // Stop all video streams.
+        this.stopWebcamStream();
+
+        this.hideVideoPlayer();
+        this.displaySnapshot();
+    },
+
+    getSnapshot: function () {
+        return this.snapshotCanvasElement.toDataURL('image/jpeg', 1.0);
+    },
+
+    stopWebcamStream: function () {
+        this.videoTracks.forEach(function (track) {
+            track.stop();
+        });
+    },
+
+    displayVideoPlayer: function () {
+        this.show(this.videoPlayerElement);
+    },
+
+    hideVideoPlayer: function () {
+        this.hide(this.videoPlayerElement);
+    },
+
+    displaySnapshot: function () {
+        this.show(this.snapshotCanvasElement);
+    },
+
+    hideSnapshot: function () {
+        this.hide(this.snapshotCanvasElement);
+    },
+
+    hideWebcamAccessErrorSection: function () {
+        this.hide(this.webcamAccessErrorSectionElement);
+    },
+
+    handleWebcamAccessError: function (error) {
+        if (error.name === "PermissionDeniedError") {
+            this.show(this.webcamAccessErrorSectionElement);
+        }
+    }
+};
