c692c4dedecc78b4c94967cf6d66568707194673
rba\jwilkerson <jason.wilkerson@rbaconsulting.com>
2017-03-04 15:09:51 -0600

    Updating VerifyPerson implementation

---
+++ src/Feature/CognativeServices/code/Models/Repository/IdentifyResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.CognitiveServices.Models.Repository
+{
+   using Microsoft.ProjectOxford.Face.Contract;
+   using Newtonsoft.Json;
+
+   [Serializable]
+   public class IdentifyResponse
+   {
+      [JsonProperty("faceId")]
+      public string FaceId { get; set; }
+
+      [JsonProperty("candidates")]
+      public List<Candidate> Candidates { get; set; }
+   }
+}
